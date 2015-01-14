4d1389b00532247cb8e7f6160c28e87621e35ea0
Alexander Altman <alexanderaltman@me.com>
2015-01-14 15:21:56 -0800

    Beginning of tests

---
+++ AquiferTests/ProxyTests.swift
@@ -0,0 +1,11 @@
+//
+//  ProxyTests.swift
+//  Aquifer
+//
+//  Created by Alexander Ronald Altman on 1/14/15.
+//  Copyright (c) 2015 TypeLift. All rights reserved.
+//
+
+import Foundation
+import Swiftz
+import Aquifer
