5b2b5d8f386b337abd558a0bd8dcf245c375d4a3
Alexander Altman <alexanderaltman@me.com>
2015-01-13 22:27:18 -0800

    Implement `observe`

--- Aquifer/Proxy.swift
+++ Aquifer/Proxy.swift
@@ -7,18 +7,31 @@
 //
 
 import Foundation
+import Swiftz_core
 import Swiftz
+
+internal enum ProxyRepr<UO, UI, DI, DO, FR> {
+    case Request(UO, UI -> ProxyRepr<UO, UI, DI, DO, FR>)
+    case Respond(DO, DI -> ProxyRepr<UO, UI, DI, DO, FR>)
+    case Pure(FR)
+
+    internal func observe() -> ProxyRepr<UO, UI, DI, DO, FR> {
+        switch self {
+        case let Request(uO, fUI): return Request(uO) { fUI($0).observe() }
+        case let Respond(dO, fDI): return Respond(dO) { fDI($0).observe() }
+        case Pure(_): return self
+        }
+    }
+}
 
 public struct Proxy<UO, UI, DI, DO, FR> {
     private let repr: ProxyRepr<UO, UI, DI, DO, FR>
 
-    internal init(_ repr: ProxyRepr<UO, UI, DI, DO, FR>) {
-        self.repr = repr
+    internal init(_ r: ProxyRepr<UO, UI, DI, DO, FR>) {
+        repr = r
     }
-}
 
-internal enum ProxyRepr<UO, UI, DI, DO, FR> {
-    case Request(() -> UO, UI -> ProxyRepr<UO, UI, DI, DO, FR>)
-    case Respond(() -> DO, DI -> ProxyRepr<UO, UI, DI, DO, FR>)
-    case Pure(FR)
+    public func observe() -> Proxy<UO, UI, DI, DO, FR> {
+        return Proxy(repr.observe())
+    }
 }
