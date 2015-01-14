cc62a2dae01a0c66cb621d09f5bcd530f69d1090
Alexander Altman <alexanderaltman@me.com>
2015-01-13 17:35:35 -0800

    Initial Commit

---
+++ Aquifer/Aquifer.h
@@ -0,0 +1,19 @@
+//
+//  Aquifer.h
+//  Aquifer
+//
+//  Created by Alexander Ronald Altman on 1/13/15.
+//  Copyright (c) 2015 TypeLift. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Aquifer.
+FOUNDATION_EXPORT double AquiferVersionNumber;
+
+//! Project version string for Aquifer.
+FOUNDATION_EXPORT const unsigned char AquiferVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Aquifer/PublicHeader.h>
+
+
