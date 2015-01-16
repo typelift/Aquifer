091f8644dc105413d5bb0ad234962ba3b6ad2961
Alexander Altman <alexanderaltman@me.com>
2015-01-16 14:08:48 -0800

    restart

---
+++ Aquifer/RawIO.swift
@@ -0,0 +1,13 @@
+//
+//  RawIO.swift
+//  Aquifer
+//
+//  Created by Alexander Ronald Altman on 1/16/15.
+//  Copyright (c) 2015 TypeLift. All rights reserved.
+//
+
+import Foundation
+import Swiftz
+
+class RawProducerImpl: NSInputStream {
+}
