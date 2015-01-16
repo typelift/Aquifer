0c93476ea4b8731f68dc4953c36290d70088b9b1
Alexander Altman <alexanderaltman@me.com>
2015-01-15 18:24:49 -0800

    Begin input

---
+++ Aquifer/Input.swift
@@ -0,0 +1,13 @@
+//
+//  StringInput.swift
+//  Aquifer
+//
+//  Created by Alexander Ronald Altman on 1/15/15.
+//  Copyright (c) 2015 TypeLift. All rights reserved.
+//
+
+import Foundation
+import Swiftz
+
+public func producer<UO, UI>(fromFileHandle handle: NSFileHandle, withChuckSize chunkSize: Int = 128) -> Proxy<UO, UI, (), NSData, ()> {
+}
