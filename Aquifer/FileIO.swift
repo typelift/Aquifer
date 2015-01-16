bcabd12f1171a35676ff64a603a3704bf6fc7b79
Alexander Altman <alexanderaltman@me.com>
2015-01-16 14:55:28 -0800

    Progress?

--- Aquifer/FileIO.swift
+++ Aquifer/FileIO.swift
@@ -9,4 +9,21 @@
 import Foundation
 import Swiftz
 
+public func readLines<UO, UI>(fromURL url: NSURL, withChunkSize chunkSize: Int = 64) -> Proxy<UO, UI, (), String, ()>? {
+    if let stream: NSInputStream = NSInputStream(URL: url) {
+        var buffer: [UInt8] = Array(count: chunkSize, repeatedValue: 0)
+        buffer.withUnsafeMutableBufferPointer({ (inout ptr: UnsafeMutableBufferPointer<UInt8>) in
+            stream.read(ptr.baseAddress, maxLength: chunkSize)
+        })
+    } else {
+        return nil
+    }
+}
 
+public func readLines<UO, UI>(fromFileAtPath path: String) -> Proxy<UO, UI, (), String, ()>? {
+    if let url = NSURL(fileURLWithPath: path) {
+        return readLines(fromURL: url)
+    } else {
+        return nil
+    }
+}
