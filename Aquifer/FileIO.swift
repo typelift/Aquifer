09a39ed185da411414c9092e7e3ec057dabf8831
Alexander Altman <alexanderaltman@me.com>
2015-01-16 19:39:26 -0800

    LineReader

--- Aquifer/FileIO.swift
+++ Aquifer/FileIO.swift
@@ -9,15 +9,7 @@
 import Foundation
 import Swiftz
 
-public func readLines<UO, UI>(fromURL url: NSURL, withChunkSize chunkSize: Int = 64) -> Proxy<UO, UI, (), String, ()>? {
-    if let stream: NSInputStream = NSInputStream(URL: url) {
-        var buffer: [UInt8] = Array(count: chunkSize, repeatedValue: 0)
-        buffer.withUnsafeMutableBufferPointer({ (inout ptr: UnsafeMutableBufferPointer<UInt8>) in
-            stream.read(ptr.baseAddress, maxLength: chunkSize)
-        })
-    } else {
-        return nil
-    }
+public func readLines<UO, UI>(fromURL url: NSURL) -> Proxy<UO, UI, (), String, ()>? {
 }
 
 public func readLines<UO, UI>(fromFileAtPath path: String) -> Proxy<UO, UI, (), String, ()>? {
