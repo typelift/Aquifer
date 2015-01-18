0c60939a42a34903bbe02d0d86d4ed203d09863a
Alexander Altman <alexanderaltman@me.com>
2015-01-18 15:54:50 -0800

    grouping and parsing to start

---
+++ Aquifer/Grouping.swift
@@ -0,0 +1,10 @@
+//
+//  Grouping.swift
+//  Aquifer
+//
+//  Created by Alexander Ronald Altman on 1/18/15.
+//  Copyright (c) 2015 TypeLift. All rights reserved.
+//
+
+import Foundation
+import Swiftz
