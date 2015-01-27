e18bf2c1b7a46876b4427545fecd9960649fda7c
Alexander Altman <alexanderaltman@me.com>
2015-01-27 15:29:13 -0800

    `groupsBy`, at last¡|

--- Aquifer/Grouping.swift
+++ Aquifer/Grouping.swift
@@ -11,21 +11,39 @@ import Swiftz
 
 internal enum GroupedProducerSignature<V, R> {
     case End(() -> R)
-    case More(() -> Proxy<X, (), (), V, GroupedProducer<V, R>>)
+    case More(() -> Proxy<X, (), (), V, GroupedProducerSignature<V, R>>)
 }
 
 public struct GroupedProducer<V, R> {
-    internal let underlying: GroupedProducerSignature<V, R>
+    internal let underlying: () -> GroupedProducerSignature<V, R>
 
-    internal init(_ u: GroupedProducerSignature<V, R>) {
+    internal init(_ u: @autoclosure () -> GroupedProducerSignature<V, R>) {
         underlying = u
     }
 }
 
+private func groupsBySignature<V, R>(p: Proxy<X, (), (), V, R>, equals: (V, V) -> Bool) -> GroupedProducerSignature<V, R> {
+    switch next(p) {
+    case let .Left(x): return GroupedProducerSignature.End { _ in x.value }
+    case let .Right(k):
+        let (dO, q) = k.value
+        return GroupedProducerSignature.More { _ in
+            groupsBySignature(
+                span(
+                    yield(dO) >>- { _ in
+                        q
+                    }
+                    ) { v in
+                        equals(dO, v)
+                }, equals)
+        }
+    }
+}
+
 public func groupsBy<V, R>(p: Proxy<X, (), (), V, R>, equals: (V, V) -> Bool) -> GroupedProducer<V, R> {
-    return 
+    return GroupedProducer(groupsBySignature(p, equals))
 }
 
 public func groups<V: Equatable, R>(p: Proxy<X, (), (), V, R>) -> GroupedProducer<V, R> {
-    return groupsBy(p, ==)
+    return groupsBy(p) { v0, v1 in v0 == v1  }
 }
