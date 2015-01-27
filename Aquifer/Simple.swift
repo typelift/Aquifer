7e687cfc4a34960e18de75d752af9b9309195d52
Alexander Altman <alexanderaltman@me.com>
2015-01-26 21:39:02 -0800

    finally it compiles !

--- Aquifer/Simple.swift
+++ Aquifer/Simple.swift
@@ -12,7 +12,7 @@ import Swiftz
 public func next<DO, FR>(p: Proxy<X, (), (), DO, FR>) -> Either<FR, (DO, Proxy<X, (), (), DO, FR>)> {
     switch p.repr {
     case let .Request(uO, _): return closed(uO)
-    case let .Respond(dO, fDI): return .Right((dO, fDI(())))
-    case let .Pure(x): return Left(x())
+    case let .Respond(dO, fDI): return .Right(Box((dO(), Proxy(fDI(())))))
+    case let .Pure(x): return .Left(Box(x()))
     }
 }
