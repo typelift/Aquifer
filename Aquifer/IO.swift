e78d9b8ee0d0fff4a8f31f03349a310a2fab7682
Alexander Altman <alexanderaltman@me.com>
2015-01-28 11:10:15 -0800

    `replicate`

--- Aquifer/IO.swift
+++ Aquifer/IO.swift
@@ -26,3 +26,9 @@ public func fromHandle<UO, UI>(handle: NSFileHandle) -> Proxy<UO, UI, (), String
 public func repeat<UO, UI, DO, FR>(v: @autoclosure () -> DO) -> Proxy<UO, UI, (), DO, FR> {
     return pure(v) >~ cat()
 }
+
+public func replicate<UO, UI, DO, FR>(v: @autoclosure () -> DO, n: Int) -> Proxy<UO, UI, (), DO, FR> {
+    return pure(v) >~ take(n)
+}
+
+public
