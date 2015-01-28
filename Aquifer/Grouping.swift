d9c42c9278cb74c9ae93ad73766489769199638d
Alexander Altman <alexanderaltman@me.com>
2015-01-27 20:00:20 -0800

    underlying -> repr

--- Aquifer/Grouping.swift
+++ Aquifer/Grouping.swift
@@ -9,25 +9,27 @@
 import Foundation
 import Swiftz
 
-internal enum GroupedProducerSignature<V, R> {
+internal enum GroupedProducerRepr<V, R> {
     case End(() -> R)
-    case More(() -> Proxy<X, (), (), V, GroupedProducerSignature<V, R>>)
+    case More(() -> Proxy<X, (), (), V, GroupedProducerRepr<V, R>>)
 }
 
 public struct GroupedProducer<V, R> {
-    internal let underlying: () -> GroupedProducerSignature<V, R>
+    internal let repr: () -> GroupedProducerRepr<V, R>
 
-    internal init(_ u: @autoclosure () -> GroupedProducerSignature<V, R>) {
-        underlying = u
+    internal init(_ r: @autoclosure () -> GroupedProducerRepr<V, R>) {
+        repr = r
     }
 }
 
-private func groupsBySignature<V, R>(p: Proxy<X, (), (), V, R>, equals: (V, V) -> Bool) -> GroupedProducerSignature<V, R> {
+public func delay(p: @autoclosure () -> GroupedProducer)
+
+private func groupsBySignature<V, R>(p: Proxy<X, (), (), V, R>, equals: (V, V) -> Bool) -> GroupedProducerRepr<V, R> {
     switch next(p) {
-    case let .Left(x): return GroupedProducerSignature.End { _ in x.value }
+    case let .Left(x): return GroupedProducerRepr.End { _ in x.value }
     case let .Right(k):
         let (dO, q) = k.value
-        return GroupedProducerSignature.More { _ in { r in groupsBySignature(r, equals) } <^> (span(yield(dO) >>- { _ in q }) { v in equals(dO, v) }) }
+        return GroupedProducerRepr.More { _ in { r in groupsBySignature(r, equals) } <^> (span(yield(dO) >>- { _ in q }) { v in equals(dO, v) }) }
     }
 }
 
@@ -37,4 +39,10 @@ public func groupsBy<V, R>(p: Proxy<X, (), (), V, R>, equals: (V, V) -> Bool) ->
 
 public func groups<V: Equatable, R>(p: Proxy<X, (), (), V, R>) -> GroupedProducer<V, R> {
     return groupsBy(p) { v0, v1 in v0 == v1  }
+}
+
+public func chunksOf<V, R>(p: Proxy<X, (), (), V, R>, n: Int) -> GroupedProducer<V, R> {
+    switch next(p) {
+
+    }
 }
