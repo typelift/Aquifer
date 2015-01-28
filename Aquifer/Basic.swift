082c660e9669a2c085c639658c65d0daf410a7b9
Alexander Altman <alexanderaltman@me.com>
2015-01-28 11:35:12 -0800

    `take`

--- Aquifer/Basic.swift
+++ Aquifer/Basic.swift
@@ -23,10 +23,10 @@ public func repeat<UO, UI, DO, FR>(v: () -> DO) -> Proxy<UO, UI, (), DO, FR> {
     return once(v) >~ take(n)
 }*/
 
-public func take<UI, DO, FR>(n: Int) -> Proxy<(), UI, (), DO, ()> {
+public func take<DT>(n: Int) -> Proxy<(), DT, (), DT, ()> {
     if n <= 0 {
         return pure(())
     } else {
-        return await() >>- { yield($0) >>- { _ in take(n - 1) } }
-    }
+        return await() >>- { yield($0) >>- { _ in take(n - 1) }
+ }    }
 }
