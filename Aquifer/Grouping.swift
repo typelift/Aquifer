abc00e0df9448da266aec14bc62a0224f956fae9
Alexander Altman <alexanderaltman@me.com>
2015-01-27 20:12:24 -0800

    more repr stuff

--- Aquifer/Grouping.swift
+++ Aquifer/Grouping.swift
@@ -30,17 +30,17 @@ public func delay<V, R>(p: @autoclosure () -> GroupedProducer<V, R>) -> GroupedP
     return GroupedProducer(p().repr)
 }
 
-private func groupsBySignature<V, R>(p: Proxy<X, (), (), V, R>, equals: (V, V) -> Bool) -> GroupedProducerRepr<V, R> {
+private func groupsByRepr<V, R>(p: Proxy<X, (), (), V, R>, equals: (V, V) -> Bool) -> GroupedProducerRepr<V, R> {
     switch next(p) {
     case let .Left(x): return GroupedProducerRepr.End { _ in x.value }
     case let .Right(k):
         let (dO, q) = k.value
-        return GroupedProducerRepr.More { _ in { r in groupsBySignature(r, equals) } <^> (span(yield(dO) >>- { _ in q }) { v in equals(dO, v) }) }
+        return GroupedProducerRepr.More { _ in { r in groupsByRepr(r, equals) } <^> (span(yield(dO) >>- { _ in q }) { v in equals(dO, v) }) }
     }
 }
 
 public func groupsBy<V, R>(p: Proxy<X, (), (), V, R>, equals: (V, V) -> Bool) -> GroupedProducer<V, R> {
-    return GroupedProducer(groupsBySignature(p, equals))
+    return GroupedProducer(groupsByRepr(p, equals))
 }
 
 public func groups<V: Equatable, R>(p: Proxy<X, (), (), V, R>) -> GroupedProducer<V, R> {
