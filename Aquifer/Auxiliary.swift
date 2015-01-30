b14ea9f40446ee5b9072044578e94e8fff8329b6
Alexander Altman <alexanderaltman@me.com>
2015-01-29 22:07:19 -0800

    `right`

--- Aquifer/Auxiliary.swift
+++ Aquifer/Auxiliary.swift
@@ -27,3 +27,16 @@ private func leftInner<A, B, C>() -> Proxy<(), Either<A, C>, (), Either<B, C>, A
 public func left<A, B, C, R>(p: Proxy<(), A, (), B, R>) -> Proxy<(), Either<A, C>, (), Either<B, C>, R> {
     return leftInner() >~ for_(p) { v in yield(Either.left(v)) }
 }
+
+private func rightInner<A, B, C>() -> Proxy<(), Either<C, A>, (), Either<C, B>, A> {
+    return await() >>- {
+        switch $0 {
+        case let .Left(x): return yield(Either.left(x.value)) >>- { _ in rightInner() }
+        case let .Right(y): return pure(y.value)
+        }
+    }
+}
+
+public func right<A, B, C, R>(p: Proxy<(), A, (), B, R>) -> Proxy<(), Either<C, A>, (), Either<C, B>, R> {
+    return rightInner() >~ for_(p) { v in yield(Either.right(v)) }
+}
