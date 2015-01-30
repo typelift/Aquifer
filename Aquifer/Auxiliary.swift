e880fcb465f390417815f2d6e896033e9bb4a9ac
Alexander Altman <alexanderaltman@me.com>
2015-01-29 22:04:15 -0800

    `left`

--- Aquifer/Auxiliary.swift
+++ Aquifer/Auxiliary.swift
@@ -14,3 +14,16 @@ import Swiftz
 public func arr<A, B, R>(f: A -> B) -> Proxy<(), A, (), B, R> {
     return map(f)
 }
+
+private func leftInner<A, B, C>() -> Proxy<(), Either<A, C>, (), Either<B, C>, A> {
+    return await() >>- {
+        switch $0 {
+        case let .Left(x): return pure(x.value)
+        case let .Right(y): return yield(Either.right(y.value)) >>- { _ in leftInner() }
+        }
+    }
+}
+
+public func left<A, B, C, R>(p: Proxy<(), A, (), B, R>) -> Proxy<(), Either<A, C>, (), Either<B, C>, R> {
+    return leftInner() >~ for_(p) { v in yield(Either.left(v)) }
+}
