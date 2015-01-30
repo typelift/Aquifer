8322be4f40ab1500ca6f953a6b2e496695f3466b
Alexander Altman <alexanderaltman@me.com>
2015-01-29 21:48:24 -0800

    `Auxiliary.swift`

---
+++ Aquifer/Auxiliary.swift
@@ -0,0 +1,13 @@
+//
+//  Auxiliary.swift
+//  Aquifer
+//
+//  Created by Alexander Ronald Altman on 1/29/15.
+//  Copyright (c) 2015 TypeLift. All rights reserved.
+//
+
+// roughly `Pipes.Extras`
+
+import Foundation
+import Swiftz
+
