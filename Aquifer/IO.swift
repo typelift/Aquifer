c66fb710c2013b737ace0e3d70390a18012b238f
Robert Widmann <devteam.codafi@gmail.com>
2015-08-14 21:21:29 -0600

    Document IO

--- Aquifer/IO.swift
+++ Aquifer/IO.swift
@@ -11,10 +11,13 @@
 import Foundation
 import Swiftz
 
+/// Returns a pipe that reads input from `stdin` line-by-line and terminates on end-of-input.
 public func stdinLn<UO, UI>() -> Proxy<UO, UI, (), String, ()> {
     return fromHandle(NSFileHandle.fileHandleWithStandardInput())
 }
 
+/// Returns a pipe that reads input from the given handle line-by-line and terminates on 
+/// end-of-input.
 public func fromHandle<UO, UI>(handle: NSFileHandle) -> Proxy<UO, UI, (), String, ()> {
     if handle.isAtEndOfFile {
         return pure(())
@@ -23,22 +26,28 @@ public func fromHandle<UO, UI>(handle: NSFileHandle) -> Proxy<UO, UI, (), String
     }
 }
 
+/// Returns a pipe that writes output to `stdout` line-by-line and terminates on end-of-input.
 public func stdoutLn<DI, DO, FR>() -> Proxy<(), String, DI, DO, FR> {
     return toHandle(NSFileHandle.fileHandleWithStandardOutput())
 }
 
+/// Returns a pipe that writes output to the given handle line-by-line and terminates on 
+/// end-of-input.
 public func toHandle<DI, DO, FR>(handle: NSFileHandle) -> Proxy<(), String, DI, DO, FR> {
     return for_(cat()) { handle.writeLine($0); return pure(()) }
 }
 
+/// Returns a pipe that prints the description of input values to `stdout`.
 public func describe<UI: CustomStringConvertible, DI, DO, FR>() -> Proxy<(), UI, DI, DO, FR> {
     return description() >-> stdoutLn()
 }
 
+/// Returns a pipe that prints the debug description of input values to `stdout`.
 public func debugDescribe<UI: CustomDebugStringConvertible, DI, DO, FR>() -> Proxy<(), UI, DI, DO, FR> {
     return debugDescription() >-> stdoutLn()
 }
 
+/// Returns a pipe that prints the streamable data of input values to the given output stream.
 public func writeTo<DT: Streamable, OS: OutputStreamType, FR>(inout stream: OS) -> Proxy<(), DT, (), DT, FR> {
     return chain { $0.writeTo(&stream) }
 }
