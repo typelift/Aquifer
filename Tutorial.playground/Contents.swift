2f7fb1d4a30a7e886cab9d3fa7d5dca09367bd41
Robert Widmann <devteam.codafi@gmail.com>
2015-08-27 10:14:29 -0400

    Add BSD3 attribution

--- Tutorial.playground/Contents.swift
+++ Tutorial.playground/Contents.swift
@@ -1,3 +1,29 @@
+// Copyright (c) 2012-2014 Gabriel Gonzalez
+// All rights reserved.
+//	
+// Redistribution and use in source and binary forms, with or without modification,
+// are permitted provided that the following conditions are met:
+// * Redistributions of source code must retain the above copyright notice,
+// this list of conditions and the following disclaimer.
+// * Redistributions in binary form must reproduce the above copyright notice,
+// this list of conditions and the following disclaimer in the documentation
+// and/or other materials provided with the distribution.
+// * Neither the name of Gabriel Gonzalez nor the names of other contributors
+// may be used to endorse or promote products derived from this software
+// without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+// ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+// ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+
 // MARK: - Introduction
 
 //: The `Aquifer` library decouples stream processing stages from each other so
@@ -107,10 +133,10 @@ public func stdinByLine() -> Producer<String, ()>.T {
 //: `for_` loops are the simplest way to consume a `Producer` like `stdinByLine`.
 //: `for_` has the following type:
 //:
-//                        +-- Producer      +-- The body of the   +-- Result
-//                        |   to loop       |   loop              |
-//                        v   over          v                     v
-//                        --------------       ---------------     ---------
+//                        +-- Producer           +-- The body of the   +-- Result
+//                        |   to loop            |   loop              |
+//                        v   over               v                     v
+//                        --------------         ---------------       ---------
 //     func for_<A, R>(p: Producer<A, R>.T, _ f: A -> Effect<()>.T) -> Effect<R>.T
 //
 //: `for_(producer, body)` loops over `producer`, substituting each `yield` in
